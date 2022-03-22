0b98a48401382abaf4e7751d21f14408c05e255f
ataricco <ataricco@wpi.edu>
2022-03-22 08:09:53 -0400

    Added Test Activity called MainActivityAngTest

---
+++ app/src/main/java/com/example/mygittestapp/MainActivityAngTest.java
@@ -0,0 +1,14 @@
+package com.example.mygittestapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityAngTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_ang_test);
+    }
+}
