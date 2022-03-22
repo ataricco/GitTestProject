c3ac84d97eced4d4d61aa0dc1550211b370221ab
mtaricco <mtaricco@gmail.com>
2022-03-22 08:40:06 -0400

    i added comment in line 8

--- app/src/main/java/com/example/mygittestapp/MainActivityAngTest.java
+++ app/src/main/java/com/example/mygittestapp/MainActivityAngTest.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivityAngTest extends AppCompatActivity {
-//mark was here
+//mark was here and now added more stuff p
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_ang_test);
     }
-}
+    protected void onStop(Bundle savedInstanceState) {
+
+    }
+    }
